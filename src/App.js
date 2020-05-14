b4c7ccd128993923ce595cfbba65c0e59a06688c
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-05-13 20:42:59 -0700

    Added Modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 const App = () => {
+  const themeHook = useState("green");
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/"> Adopt Me! </Link>
-        </header>
-        <Router>
-          {/* order of declaration doesn't matter in reach router */}
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/"> Adopt Me! </Link>
+          </header>
+          <Router>
+            {/* order of declaration doesn't matter in reach router */}
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
