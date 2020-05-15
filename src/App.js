ab8416e6ee16ed2ebc44f7427b870f197dcaf0e1
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-05-14 19:35:31 -0700

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+import NavBar from "./Navbar";
 
 const App = () => {
   const themeHook = useState("green");
@@ -13,9 +14,7 @@ const App = () => {
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
         <div>
-          <header>
-            <Link to="/"> Adopt Me! </Link>
-          </header>
+          <NavBar />
           <Router>
             {/* order of declaration doesn't matter in reach router */}
             <SearchParams path="/" />
